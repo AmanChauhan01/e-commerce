bb610c78fea7a005c51bc0b4d2e4c0b01f5000d4
AmanChauhan01 <129518568+AmanChauhan01@users.noreply.github.com>
2023-06-05 17:47:13 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,73 @@
+const wrapper=document.querySelector(".wrap7 .wrapper");
+const carousels=document.querySelector(".wrap7 .carousel");
+const arrowBtns=document.querySelectorAll(".wrap7 .wrapper i");
+const firstCardWidth=carousels.querySelector(".wrap7 .cards").offsetWidth;
+const carouselChildrens=[...carousels.children]
+
+let isDragging = false, startX, startScrollLeft, timeoutId;
+
+let cardPerView = Math.round(carousels.offsetWidth / firstCardWidth);
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousels.insertAdjacentHTML("afterbegin", card.outerHTML)
+})
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousels.insertAdjacentHTML("beforeend", card.outerHTML)
+})
+
+arrowBtns.forEach(btn =>{
+    btn.addEventListener("click", () =>{
+        // console.log(btn.id)
+        carousels.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth
+    })
+})
+
+const dragStart = (e) =>{
+    isDragging = true;
+    carousels.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousels.scrollLeft
+}
+
+const dragging = (e)=>{
+    // console.log(e.pageX);
+    if(!isDragging) return;
+    carousels.scrollLeft=startScrollLeft - (e.pageX - startX)
+}
+
+const dragStop = () =>{
+    isDragging=false;
+    carousels.classList.remove("dragging");
+}
+const autoPlay = ()=>{
+    if(window.innerWidth < 400) return;
+    timeoutId = setTimeout(() => carousels.scrollLeft += firstCardWidth, 1000);
+}
+autoPlay();
+
+const infiniteScroll = () =>{
+    if(carousels.scrollLeft === 0){
+        // console.log("hii")
+        carousels.classList.add("no-transition");
+        carousels.scrollLeft=carousels.scrollWidth - (2 * carousels.offsetWidth)
+        carousels.classList.remove("no-transition");
+    }
+    else if(Math.ceil(carousels.scrollLeft) === carousels.scrollWidth -carousels.offsetWidth){
+        // console.log("Bii")
+        carousels.classList.add("no-transition");
+        carousels.scrollLeft=carousels.offsetWidth;
+        carousels.classList.remove("no-transition");
+    }
+    clearTimeout(timeoutId);
+    if(!wrapper.matched(":hover")) autoPlay();
+}
+carousels.addEventListener("mousedown", dragStart);
+carousels.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousels.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
+
+document.querySelector(".click-form .reg").addEventListener("click", function(){
+    document.querySelector(".click-form .register").style.display="block"
+}) 
